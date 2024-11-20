cdfbfddcc7ebb11a6fcc9621ed5f6f7aa4c06ffc
mmmhdp <malcevnik99@gmail.com>
2024-11-20 07:17:27 +0300

    init commit

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+# Created by `pipx` on 2024-09-20 17:49:21
+set PATH $PATH /home/mal/.local/bin
+
+set -gx EDITOR      nvim
+set -gx SUDO_EDITOR nvim
+set -gx VISUAL      nvim
